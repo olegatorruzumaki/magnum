911803c60a4968eb88bcd50d4a074af428090ef7
Oleg <olegkurbatov97@mail.ru>
2021-09-07 22:50:52 +0600

    products

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,26 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+    <div id="app" class="container">
+        <router-view class="row"/>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+    name: 'App',
+    components: {},
+    data() {
+        return {
+        }
+    },
+    mounted() {
+        console.log('app');
+
+    },
+    methods: {
+
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,26 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import VueRouter from 'vue-router'
+import App from './App.vue'
+import {BootstrapVue} from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './styles/scss/main.scss'
+import Main from '@/components/Main'
+
+Vue.use(BootstrapVue)
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+const routes = [
+    {path: '/', name: 'Main', component: Main},
+]
+
+const router = new VueRouter({
+    routes
 })
+
+new Vue({
+    render: h => h(App),
+    router,
+}).$mount('#app')
